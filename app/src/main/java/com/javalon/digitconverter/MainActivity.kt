2a3694588169f19535616108d16ee36dc07d3249
ezechuka <ezechukwuka9@gmail.com>
2021-08-22 22:26:32 +0100

    added unit-tests and improved kotlin code.

--- app/src/main/java/com/javalon/digitconverter/MainActivity.kt
+++ app/src/main/java/com/javalon/digitconverter/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.javalon.digitconverter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.ezechuka.digitconverter.DigitConverter
 
 class MainActivity : AppCompatActivity() {
